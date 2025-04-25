167b70b6b6a7f0efcadb4a72c8acf810f8c13fbb
Shivarani Adla <83292756+ashivarani@users.noreply.github.com>
2025-04-25 11:35:00 +0530

    Create countNegatives.c

---
+++ countNegatives.c
@@ -0,0 +1,13 @@
+int countNegatives(int** grid, int gridSize, int* gridColSize) {
+    int count = 0;
+    for (int i = gridSize - 1; i >=0; i--) {
+       for ( int j = *gridColSize - 1; j >= 0; j--) {
+            if (grid[i][j] < 0) {
+                count++;
+            } else {
+                break;
+            }
+       }
+    }
+return count;
+}
